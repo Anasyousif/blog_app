13f30e04cf8461a9d03c44c8a1d098b27ebfc870
Anas Yousif <Anasyousif182@gmail.com>
2023-10-20 20:10:05 -0400

    today push

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,17 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>
-    <link href="/"/>Hello</div>;
+  return (
+    <div>
+      <Link href="/"><h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nulla labore voluptates cumque! Quam maiores voluptatem assumenda magnam labore nihil dolor est, exercitationem,
+       expedita porro necessitatibus, autem tempora. Nobis, tempora!</h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium saepe repudiandae atque recusandae ipsam? Asperiores impedit voluptates aliquid,
+       quae unde ipsa optio maiores obcaecati rem! Molestiae ad molestias reiciendis consectetur?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates cupiditate necessitatibus provident dignissimos? Placeat deleniti repellendus qui cum soluta libero
+        labore laboriosam facere reprehenderit sed? Consequatur rem unde molestiae exercitationem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores alias obcaecati maxime, perspiciatis, ex, repudiandae ipsum sed illo nesciunt 
+        laudantium quidem quasi aperiam ut assumenda? Magni cupiditate dolorum excepturi corrupti!</Link>
+    </div>
+  );
 }
